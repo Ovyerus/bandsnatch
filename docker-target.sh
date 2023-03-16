29755e53a8ec492ca30f0b6aee25495a545fc3f8
Michael Mitchell <ovy@ovyerus.com>
2023-03-16 22:27:53 +1100

    fix(ci): add a bloody space

--- docker-target.sh
+++ docker-target.sh
@@ -1,7 +1,7 @@
 # Script to remap Docker's `$TARGETARCH` to more appropriate values for rustup.
 if [ "$TARGETARCH" = "amd64" ]; then
     echo "x86_64"
-elif [ "$TARGETARCH" = "arm64"]; then
+elif [ "$TARGETARCH" = "arm64" ]; then
     echo "aarch64"
 else
     echo "$TARGETARCH"
