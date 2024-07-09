adf7cdffefc2ce31011e7c717e887d07c82a03b3
Ovyerus <ovy@ovyerus.com>
2024-07-09 20:42:17 +1000

    chore: use rustls instead of openssl

--- package.nix
+++ package.nix
@@ -3,14 +3,12 @@
   darwin,
   lib,
   libiconvReal,
-  openssl,
-  pkg-config,
   stdenv,
 }: let
   src = craneLib.cleanCargoSource (craneLib.path ./.);
   buildInputs = lib.optionals stdenv.isDarwin [libiconvReal];
   nativeBuildInputs =
-    [pkg-config openssl.dev]
+    []
     ++ lib.optionals stdenv.isDarwin (with darwin.apple_sdk.frameworks; [AppKit CoreFoundation]);
 in
   craneLib.buildPackage {
