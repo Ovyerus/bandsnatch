5acd4c26b5662f55ab2b86230e6dc5d623f6fbd5
Ovyerus <ovy@ovyerus.com>
2024-07-16 20:13:38 +1000

    chore: remove unused docker image

--- docker-target.sh
+++
@@ -1,8 +0,0 @@
-# Script to remap Docker's `$TARGETARCH` to more appropriate values for rustup.
-if [ "$TARGETARCH" = "amd64" ]; then
-    echo "x86_64"
-elif [ "$TARGETARCH" = "arm64" ]; then
-    echo "aarch64"
-else
-    echo "$TARGETARCH"
-fi
