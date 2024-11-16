bcd8217ff742b2721cfda39a73d1f453803626ea
Ovyerus <ovy@ovyerus.com>
2024-11-17 10:38:03 +1100

    revert(flake): no more cross compilation
    It's honestly too much of a hassle, and with GitHub finally providing Arm MacOS runners, it's not necessary for me anymore.

--- flake.nix
+++ flake.nix
@@ -13,76 +13,31 @@
     naersk,
     ...
   }: let
-    buildTargets = {
-      "x86_64-linux" = "x86_64-unknown-linux-musl";
-      "aarch64-linux" = "aarch64-unknown-linux-musl";
-      "x86_64-darwin" = "x86_64-apple-darwin";
-      "aarch64-darwin" = "aarch64-apple-darwin";
-    };
+    forSystems = fn:
+      nixpkgs.lib.genAttrs [
+        "aarch64-linux"
+        "aarch64-darwin"
+        "x86_64-darwin"
+        "x86_64-linux"
+      ] (system: fn nixpkgs.legacyPackages.${system});
+    defaultForSystems = fn: forSystems (pkgs: {default = fn pkgs;});
 
-    systems = builtins.attrNames buildTargets;
-
-    # forSystems [...system] (system: ...)
-    forSystems = systems: fn:
-      nixpkgs.lib.genAttrs systems (system: fn system);
-
-    # crossForSystems [...system] (hostSystem: targetSystem: ...)
-    crossForSystems = systems: fn:
-      forSystems systems (
-        hostSystem:
-          builtins.foldl'
-          (acc: targetSystem:
-            acc
-            // {
-              "cross-${targetSystem}" = fn hostSystem targetSystem;
-            })
-          {default = fn hostSystem hostSystem;}
-          systems
-      );
-
-    mkBandsnatch = hostSystem: targetSystem: let
-      rustTarget = buildTargets.${targetSystem};
-      pkgs = import nixpkgs {system = hostSystem;};
-      pkgsCross = import nixpkgs {
-        system = hostSystem;
-        crossSystem.config = rustTarget;
-      };
-      fenixPkgs = fenix.packages.${hostSystem};
-      toolchain = fenixPkgs.combine [
-        fenixPkgs.stable.rustc
-        fenixPkgs.stable.cargo
-        fenixPkgs.targets.${rustTarget}.stable.rust-std
-      ];
-
-      naersk-lib = pkgs.callPackage naersk {
+    mkBandsnatch = pkgs: let
+      fenixPkgs = fenix.packages.${pkgs.system};
+      toolchain = fenixPkgs.stable.minimalToolchain;
+      naerskLib = pkgs.callPackage naersk {
         cargo = toolchain;
         rustc = toolchain;
       };
-      TARGET_CC = "${pkgsCross.stdenv.cc}/bin/${pkgsCross.stdenv.cc.targetPrefix}cc";
     in
-      naersk-lib.buildPackage {
+      naerskLib.buildPackage {
         src = ./.;
-        strictDeps = true;
-        doCheck = false;
-
-        inherit TARGET_CC;
-
-        CARGO_BUILD_TARGET = rustTarget;
-        CARGO_BUILD_RUSTFLAGS = [
-          "-C"
-          "target-feature=+crt-static"
-          "-C"
-          "linker=${TARGET_CC}"
-        ];
       };
   in {
-    packages = crossForSystems systems mkBandsnatch;
+    packages = defaultForSystems (pkgs: mkBandsnatch pkgs);
 
-    devShells = forSystems systems (
-      system: let
-        pkgs = nixpkgs.legacyPackages.${system};
-        fenixPkgs = fenix.packages.${system};
-      in {default = pkgs.mkShell {nativeBuildInputs = [fenixPkgs.stable.toolchain];};}
+    devShells = defaultForSystems (
+      pkgs: pkgs.mkShell {inputsFrom = [(mkBandsnatch pkgs)];}
     );
   };
 }
